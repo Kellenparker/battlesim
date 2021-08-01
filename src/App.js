051ef3f2051a73744be315841c58c57ef7e9e335
Kellen Parker <kellenrparker@gmail.com>
2021-08-01 18:43:34 -0500

    beginning to remake ui using material ui

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import Dialog from "@material-ui/core/Dialog";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from '@material-ui/core/DialogContentText';
 import Button from "@material-ui/core/Button";
-import { TextField, Box } from '@material-ui/core';
+import { TextField, Box, CardActions, ButtonGroup} from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
 import BattleHandler from './BattleHandler';
-
 
 class Main extends React.Component {
 
@@ -17,7 +20,8 @@ class Main extends React.Component {
             userArmy: this.props.userArmy, 
             compArmy: this.props.compArmy,
             simulating: false,
-            isDialogOpen: false,
+            isEditDialogOpen: false,
+            isEndDialogOpen: false,
             errorInf: false,
             errorCav: false,
             errorArt: false,
@@ -36,6 +40,7 @@ class Main extends React.Component {
         this.addArmy = this.addArmy.bind(this);
         this.removeArmy = this.removeArmy.bind(this);
         this.simulate = this.simulate.bind(this);
+        this.closeEndHandler = this.closeEndHandler.bind(this);
         this.first = true;
         this.battle = null;
         this.currentSim = false;
@@ -88,13 +93,12 @@ class Main extends React.Component {
             }
         }
         this.changed = true;
-        this.setState({isDialogOpen: true});
+        this.setState({isEditDialogOpen: true});
     }
   
-    closeHandler = () => {
-        this.state.userArmy.subtractLosses(0, 10, 10, 10, 1);
+    closeEditHandler = () => {
         this.setState({
-            isDialogOpen: false,
+            isEditDialogOpen: false,
             errorInf: false,
             errorCav: false,
             errorArt: false
@@ -108,9 +112,9 @@ class Main extends React.Component {
                !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
     }
 
-    saveHandler = () => {
+    saveEditHandler = () => {
         if(this.state.errorInf || this.state.errorCav || this.state.errorArt) return;
-        this.setState({isDialogOpen: false});
+        this.setState({isEditDialogOpen: false});
         if(this.editArmy.user){
             this.state.userArmy.editArmy(
                 parseInt(this.editArmy.index),
@@ -176,8 +180,12 @@ class Main extends React.Component {
         }
     }
 
+    closeEndHandler(){
+        this.setState({isEndDialogOpen: false});
+    }
+
     simulate(){
-        if(!this.state.simulating){
+        if(!this.state.simulating && this.result === 0){
             this.setState({simulating: true});
             this.currentSim = true;
             if(this.first) {
@@ -185,7 +193,16 @@ class Main extends React.Component {
                 this.first = false;
             }
             const handle = setInterval(() => {
-                if(!this.currentSim || this.result !== 0) clearInterval(handle);
+                if(!this.currentSim || this.result !== 0){
+                    clearInterval(handle);
+                    if(this.result !== 0){
+                        this.setState({
+                            isEndDialogOpen: true,
+                            simulating: false
+                        });
+                        this.currentSim = false;
+                    }
+                }
                 this.result = this.battle.progress(this.state.userArmy, this.state.compArmy, this.changed);
                 console.log(this.result);
                 this.changed = false;
@@ -217,7 +234,7 @@ class Main extends React.Component {
                             user={false} simulating={this.state.simulating}></Army>
                 </div>
 
-                <Dialog open={this.state.isDialogOpen} onClose={this.closeHandler}>
+                <Dialog open={this.state.isEditDialogOpen} onClose={this.closeEditHandler}>
                     <DialogTitle>{"Edit Army"}</DialogTitle>
                     <DialogContent>
                     <form className="form" noValidate autoComplete="off">
@@ -259,16 +276,29 @@ class Main extends React.Component {
                     </form>
                     </DialogContent>
                     <DialogActions>
-                    <Button onClick={this.saveHandler} 
+                    <Button onClick={this.saveEditHandler} 
                             color="primary" autoFocus>
                         Save
                     </Button>
-                    <Button onClick={this.closeHandler} 
+                    <Button onClick={this.closeEditHandler} 
                             color="primary" autoFocus>
                         Close
                     </Button>
                     </DialogActions>
-                </Dialog>       
+                </Dialog>
+
+                <Dialog open={this.state.isEndDialogOpen} onClose={this.closeEndHandler}>
+                    <DialogTitle>{"The Battle is Over"}</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>{this.result === 2 ? "Green has won." : "Red has won."}</DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                    <Button onClick={this.closeEndHandler} 
+                            color="primary" autoFocus>
+                        Close
+                    </Button>
+                    </DialogActions>
+                </Dialog>
 
                 <div id="base">
                     <Footer userArmy={this.state.userArmy} compArmy={this.state.compArmy} simulateHandler={this.simulate} simulating={this.state.simulating}></Footer>
@@ -301,23 +331,36 @@ class Army extends React.Component {
                 {
                     this.state.getArmy().map((army, index) => {
                         return (
-                            <div className={"element " + (this.props.user ? "user " : "comp ") + (this.state.getRetreating(index) ? "inactive" : "")} >
-                                <h3 className="elehead">{this.genArmyNumber(this.state.getIndex(index) + 1) + " Army"}</h3>
-                                <p className="paragraph">Infantry = {army.infantry}, Cavalry = {army.cavalry}, Artillery = {army.artillery}</p>
-                                <p className="paragraph">Skill = {army.skill}, Morale = {army.morale}</p>
-                                <button className="armyBut" onClick={() => this.props.editHandler(this.props.user, index)}
-                                    disabled={this.props.simulating || this.state.getRetreating(index)}>Edit</button>
-                                {this.state.getArmyCount() !== 1 && 
-                                    <button className="armyBut" onClick={() => this.props.removeHandler(this.props.user, index)}
-                                        disabled={this.props.simulating || this.state.getRetreating(index)}>Remove</button>
-                                }
-                            </div>
+                            <Card className="element" style={this.state.getRetreating(index) ? {backgroundColor: "#cccccc"} : this.props.user ? {backgroundColor: "#ccffcc"} : {backgroundColor: "#ffcccc"}}>
+                                <CardContent>
+                                    <Typography variant="h6">{this.genArmyNumber(this.state.getIndex(index) + 1) + " Army"}</Typography>
+                                    <Typography variant="body1">Infantry = {army.infantry}, Cavalry = {army.cavalry}, Artillery = {army.artillery}<br /></Typography>
+                                    <Typography variant="body1">Skill = {army.skill}, Morale = {army.morale}<br /></Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <ButtonGroup style={{ margin: '0 auto' }}>
+                                        <Button variant="contained" style={{backgroundColor: "#ffffff"}} onClick={() => this.props.editHandler(this.props.user, index)}
+                                            disabled={this.props.simulating || this.state.getRetreating(index)}>Edit</Button>
+                                        {this.state.getArmyCount() !== 1 && 
+                                            <Button variant="contained" style={{backgroundColor: "#ffffff"}} onClick={() => this.props.removeHandler(this.props.user, index)}
+                                                disabled={this.props.simulating || this.state.getRetreating(index)}>Remove</Button>
+                                        }
+                                    </ButtonGroup>
+                                </CardActions>
+                            </Card>
                         );
                     })
                 }
                 {this.state.getArmyCount() < 9 &&
-                    <button className="addArmy" onClick={() => this.props.addHandler(this.props.user)}
-                        disabled={this.props.simulating}>Add Army</button>
+                    <Button variant="contained" style={{
+                        transform: "translate(-50%)",
+                        left: "50%",
+                        backgroundColor: "#ffffff",
+                        width: "200px",
+                        height: "50px",
+	                    marginTop: "40px"
+                    }} onClick={() => this.props.addHandler(this.props.user)}
+                        disabled={this.props.simulating}>Add Army</Button>
                 }
             </div>
         )
@@ -342,14 +385,22 @@ class Footer extends React.Component {
         return (
             <div className="footer" id="footer">
                 <button id="gobut" onClick={this.props.simulateHandler}>{this.props.simulating ? "Pause / Edit" : "Simulate"}</button>
-                <p className="totals" id="userTotals">Totals: Infantry: {this.totalsUser[0]} Cavalry: {
-                    this.totalsUser[1]} Artillery: {this.totalsUser[2]}</p>
-                <p className="totals" id="userAverage">Average Skill: {this.totalsUser[3]} Average Morale: {this.totalsUser[4]}</p>
                 <p className="totals" id="compTotals">Totals: Infantry: {this.totalsComp[0]} Cavalry: {
                     this.totalsComp[1]} Artillery: {this.totalsComp[2]}</p>
                 <p className="totals" id="compAverage">Average Skill: {this.totalsComp[3]} Average Morale: {this.totalsComp[4]}</p>
-                <p className="losses" id="userLosses">Losses: Infantry: {this.lossesUser[0]} Cavalry: {this.lossesUser[1]} Artillery: {this.lossesUser[2]}</p>
                 <p className="losses" id="compLosses">Losses: Infantry: {this.lossesComp[0]} Cavalry: {this.lossesComp[1]} Artillery: {this.lossesComp[2]}</p>
+                <Card style={{
+                    position: 'fixed',
+                    zIndex: '0',
+                    backgroundColor: "ffffff"
+                }}>
+                    <CardContent>
+                        <p id="userTotals">Totals: Infantry: {this.totalsUser[0]} Cavalry: {
+                            this.totalsUser[1]} Artillery: {this.totalsUser[2]}</p>
+                        <p id="userAverage">Average Skill: {this.totalsUser[3]} Average Morale: {this.totalsUser[4]}</p> 
+                        <p id="userLosses">Losses: Infantry: {this.lossesUser[0]} Cavalry: {this.lossesUser[1]} Artillery: {this.lossesUser[2]}</p>
+                    </CardContent>
+                </Card>
             </div>
         )
     }
